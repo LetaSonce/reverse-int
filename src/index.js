6ef78d7dd5d1cc8b6e1ef0272146749bfaeab1ef
Leta Sonce <letasonce@gmail.com>
2024-08-27 01:01:24 +0300

    fix: add solution

--- src/index.js
+++ src/index.js
@@ -1,3 +1,9 @@
 module.exports = function reverse (n) {
-  
+  let revNum = 0
+  let num = Math.abs(n)
+  while(num > 0) {
+    revNum = revNum * 10 + num % 10
+    num = Math.floor(num / 10)
+  }
+  return revNum
 }
